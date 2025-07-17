aef464c9f99109a893727ed9280ce350983892c0
vas.triantafyllou <triantafyllou.vasileios@gmail.com>
2025-07-17 13:30:57 +0300

    feat: add more examples

--- javascript/simple-exercises.js
+++ javascript/simple-exercises.js
@@ -13,3 +13,31 @@ cities.push("Berlin")
 for (let i = 0; i < cities.length; i++) {
     console.log(cities[i])
 }
+
+const user = {
+    name: "Bill",
+    age: 35,    
+    city: "Athens",
+    hello() {
+    console.log(`Hello ${this.name}`)
+ }
+}
+user.hello()
+
+const circleArea = (r) => {
+   return Math.PI * (r ** 2)
+}
+
+console.log(circleArea(5))
+
+const arr = [1, 2, 3, 4, 5, 5, 3, 2, 6, 7, 9, 1]
+const removeDuplicates = (arr) => {
+    const uniqueItems = []
+
+    for(let item of arr) {
+        if(uniqueItems.indexOf(item) === -1) uniqueItems.push(item) 
+    }
+    return uniqueItems
+}
+
+console.log(removeDuplicates(arr))
