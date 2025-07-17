0a0160f82cfe62c8ae2316d64f4c664e966dc751
vas.triantafyllou <triantafyllou.vasileios@gmail.com>
2025-07-17 12:29:24 +0300

    feat:add exercises

---
+++ javascript/simple-exercises.js
@@ -0,0 +1,15 @@
+let userName = "Vasilis"
+console.log("Hello,",`${userName}!`)
+
+let num1 = 10
+let num2 = 20
+console.log(num1 + num2)
+
+let isTrue = true
+isTrue ? console.log("Είναι αλήθεια!") : console.log("Είναι ψευδές!")
+
+let cities = ["Athens", "London", "Paris"]
+cities.push("Berlin")
+for (let i = 0; i < cities.length; i++) {
+    console.log(cities[i])
+}
